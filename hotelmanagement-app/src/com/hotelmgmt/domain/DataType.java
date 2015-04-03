14dd2cdfe001161381d76c5fa0c0e905cb4359fb
Srinivas Kommu <srini@github.com>
2015-04-03 00:16:46 -0700

    add

---
+++ hotelmanagement-app/src/com/hotelmgmt/domain/DataType.java
@@ -0,0 +1,23 @@
+package com.hotelmgmt.domain;
+
+public class DataType 
+{
+	byte b;
+	char c;
+	short s;
+	int i;
+	long j;
+	float k;
+	double d;
+	boolean isTrue;
+	
+	
+
+	
+ public String toString()
+ {
+	 return "byte = "+b+"	char ="+c+"short "+s+"int="+ i+"long  =  "+j+""+"float="+ k+"double"+ d+"boolean "+ isTrue;
+	 
+ }
+
+}
---
+++ hotelmanagement-app/src/com/hotelmgmt/domain/DataTypeTest.java
@@ -0,0 +1,16 @@
+package com.hotelmgmt.domain;
+
+public class DataTypeTest {
+
+	public static void main(String[] args) {
+		DataType ref = new DataType();
+		System.out.println(ref);
+	
+	
+
+	
+	
+
+	}
+
+}
