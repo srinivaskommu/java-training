7da54aac47c6554d549ea8b884f5ab8b960b0f55
Srinivas Kommu <srini@github.com>
2015-04-13 14:51:50 -0700

    test
    test

--- hotelmanagement-app/src/com/hotelmgmt/domain/DataType.java
+++ hotelmanagement-app/src/com/hotelmgmt/domain/DataType.java
@@ -3,20 +3,35 @@ package com.hotelmgmt.domain;
 public class DataType 
 {
 	byte b;
-	char c;
+	Byte b1;
+	
+	
+	char c = 'a';
+	Character c1;
+	
 	short s;
+	Short s1;
+	
+	
 	int i;
+	Integer i1;
+	
 	long j;
+	
 	float k;
 	double d;
 	boolean isTrue;
+	
+	
+	
+	Integer iWrapper = 0;
 	
 	
 
 	
  public String toString()
  {
-	 return "byte = "+b+"	char ="+c+"short "+s+"int="+ i+"long  =  "+j+""+"float="+ k+"double"+ d+"boolean "+ isTrue;
+	 return "byte = "+b+"	char ="+c+"short "+s+"int="+ i+"long  =  "+j+""+"float="+ k+"double"+ d+"boolean "+ isTrue+ "iWrapper"+iWrapper;
 	 
  }
 
---
+++ hotelmanagement-app/src/com/hotelmgmt/domain/test/TestCustomerArray.java
@@ -0,0 +1,41 @@
+package com.hotelmgmt.domain.test;
+
+import java.util.Scanner;
+
+import com.hotelmgmt.domain.Customer;
+import com.hotelmgmt.service.CustomerService;
+
+public class TestCustomerArray {
+
+	public static void main(String[] args) throws Exception
+	{
+		
+		CustomerService service = new CustomerService();
+		
+		Customer[] customerList;
+//		try {
+			customerList = service.getAllCustomerDetails();
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//			
+//			e.printStackTrace();
+//		}
+		
+//		System.out.println("*********************");
+//		for (int j = 0; j<customerList.length; j++)
+//		{
+//		
+//			System.out.println(customerList [j].getfName()+"  ");
+//			System.out.print(customerList [j].getlName());
+//		}
+//		
+//		System.out.println("*********************");
+		
+		
+	
+	}
+		
+
+	
+
+}
