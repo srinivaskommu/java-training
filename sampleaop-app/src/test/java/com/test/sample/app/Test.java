30a42c06845da52898f07082b51a48b52fc64c0e
srinivaskommu <srinivaskommu@192.168.0.4>
2015-06-08 22:58:01 -0700

    test

---
+++ sampleaop-app/src/test/java/com/test/sample/app/Test.java
@@ -0,0 +1,20 @@
+package com.test.sample.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.test.sample.app.domain.EmployeeDTO;
+import com.test.sample.app.domain.EmployeeManager;
+
+public class Test {
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+ 
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        EmployeeManager manager = context.getBean(EmployeeManager.class);
+ 
+        manager.getEmployeeById(1);
+        manager.createEmployee(new EmployeeDTO());
+    }
+}
