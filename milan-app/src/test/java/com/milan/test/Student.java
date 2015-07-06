7448de28d650df4ea95e14ff0bd74d660d9e65c9
Srinivas Kommu <srini@github.com>
2015-07-05 22:32:04 -0700

    tests

---
+++ milan-app/src/test/java/com/milan/test/Student.java
@@ -0,0 +1,27 @@
+package com.milan.test;
+
+public class Student 
+{
+
+
+	private String name;
+	private String cwid;
+	
+	public Student(String name, String cwid) 
+	{
+		super();
+		this.name = name;
+		this.cwid = cwid;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public String getCwid() {
+		return cwid;
+	}
+
+
+
+
+}
