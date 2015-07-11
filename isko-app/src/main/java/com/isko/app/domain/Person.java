8c07821fc454d7896031ca34ad4d5d765a909ae7
Srinivas Kommu <srini@github.com>
2015-07-11 06:20:58 -0700

    person domain class with three attributes. This will be acted as super class.

---
+++ isko-app/src/main/java/com/isko/app/domain/Person.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.isko.app.domain;
+
+/**
+ * @author srinivas_kommu
+ *
+ */
+public class Person 
+{
+	private String firstName;
+	private String lastName;
+	private char gender;
+	
+	
+
+}
