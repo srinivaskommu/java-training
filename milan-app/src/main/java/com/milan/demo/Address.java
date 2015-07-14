7ffed13a418f66213cafbf8b4fc1a5047b6f659c
Srinivas Kommu <srini@github.com>
2015-07-14 07:11:19 -0700

    hashCode() examples

---
+++ milan-app/src/main/java/com/milan/demo/Address.java
@@ -0,0 +1,20 @@
+package com.milan.demo;
+
+public class Address 
+{
+
+	private String street;
+	private String city;
+	private String state;
+	private String zipCode;
+	
+	
+	public Address(String street, String city, String state, String zipCode) {
+		super();
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+	}
+
+}
